0986dd1af542e5758a9dae9b5b008a288bc894a0
wuxiao <1291383707@qq.com>
2019-08-25 22:15:18 +0800

    promise

--- main.js
+++ main.js
@@ -6,45 +6,44 @@ window.jQuery = function(nodeOrSelector){
 }
 window.$ =  window.jQuery
 
-window.jQuery.ajax = function({url,method,successFn,failFn,headers}){
-  let request = new XMLHttpRequest()
-  request.open(method,url)
-  for(let key in headers){//?
-    let value = headers[key]
-    request.setRequestHeader(key,value)//?
+window.promise = function(fn){
+  return {
+    then: function(){}
   }
-    request.onreadystatechange = ()=>{
-      if(request.readyState === 4){
-        if(request.status >=200 && request.status <300){
-          successFn.call(undefined,request.responseText)
-        }else if(request.status >=400){
-          failFn.call(undefined,request)
+}
+window.jQuery.ajax = function({url,method,successFn,failFn,headers}){
+  return new Promise(function(resolve,reject){
+    let request = new XMLHttpRequest()
+    request.open(method, url)
+    for (let key in headers) {//?
+      let value = headers[key]
+      request.setRequestHeader(key, value)//?
+    }
+    request.onreadystatechange = () => {
+      if (request.readyState === 4) {
+        if (request.status >= 200 && request.status < 300) {
+          resolve.call(undefined, request.responseText)
+        }else if (request.status >= 400) {
+          reject.call(undefined, request)
         }
       }
     }
     request.send()
+  })
 }
-
-function f1(responseText){}
-function f2(responseText){}
 
 
 myButton.addEventListener('click', (e) => {
-  window.jQuery.ajax({
-    url: '/frank',
-    method: 'get',
-    headers: {
-      'content-type': 'application/x-www-form-urlencoded',
-      'wx': '18' 
-    },
-    successFn: (x)=>{
-      f1.call(undefined,x)
-      f2.call(undefined,x)
-    },
-    failFn: (x)=>{
-      console.log(x)
-      console.log(x.status)
-      console.log(x.responseText)
-    }
-  })
+   let promise = window.jQuery.ajax({
+     url: '/xxx',
+     method: 'get',
+     headers: {
+       'content-type': 'application/x-www-form-urlencoded',
+       'wx':'18'
+     }
+   })
+    promise.then(
+      (text)=>{console.log(text)},
+      (request)=>{console.log(request)}
+    )
 })
